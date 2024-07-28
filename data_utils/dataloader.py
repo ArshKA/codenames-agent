1351233176fdae7399f6ec9f0cf5fa15115779e0
arshkon <arshkon@gmail.com>
2024-07-28 13:30:59 -0700

    remove special tokens

--- data_utils/dataloader.py
+++ data_utils/dataloader.py
@@ -8,7 +8,7 @@ class CodenamesGenerator(Dataset):
         self.end_word_index = end_word_index
         self.board_size = board_size
         self.max_guesses = max_guesses
-        self.token_size = board_size + 2
+        self.token_size = board_size
         self.device = device
 
     def __len__(self):
@@ -18,9 +18,7 @@ class CodenamesGenerator(Dataset):
         word_tensor = torch.randint(self.start_word_index, self.end_word_index, size=(self.token_size,), device=self.device)
         n = torch.randint(1, self.max_guesses+1, (1,), device=self.device).item()
         class_tensor = torch.zeros(self.token_size, dtype=torch.int, device=self.device)
-        word_tensor[0], word_tensor[1] = 0, 1
-        class_tensor[0], class_tensor[1] = 2, 3
-        indices = torch.randperm(self.token_size - 2, device=self.device) + 2
+        indices = torch.randperm(self.token_size, device=self.device)
         indices = indices[:n]
         class_tensor[indices] = 1
         return word_tensor, class_tensor
